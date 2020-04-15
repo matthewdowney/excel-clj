09d80950e18ed3c348e8e381245f0e04a45def11
Matthew Downey <matthewdowney20@gmail.com>
2020-04-15 11:14:13 -0500

    Bump to v1.3.2

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ common sense styling.
 
 Lein:
 ```
-[org.clojars.mjdowney/excel-clj "1.3.1"]
+[org.clojars.mjdowney/excel-clj "1.3.2"]
 ```
 
 - [Getting Started](#getting-started)
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mjdowney/excel-clj "1.3.1"
+(defproject org.clojars.mjdowney/excel-clj "1.3.2"
   :description "Generate Excel documents & PDFs from Clojure data."
   :url "https://github.com/matthewdowney/excel-clj"
   :license {:name "Eclipse Public License"
